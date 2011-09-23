c94114ae3f973bb83e6b936c9a4eea75da7658a0
Guy Bertrand <guybertrand101@gmail.com>
2011-09-22 21:07:09 -0400

    gamma.java

---
+++ src/gamma.java
@@ -0,0 +1,14 @@
+package ca.uqam.mgl7361.a2011.gamma.hello;
+
+public class Bonjour {
+    	public String bonjour(String nom) {
+            return String.format("Bonjour %s!", nom);
+	}
+
+	public static void main (String... args) {
+		Bonjour b = new Bonjour();
+		System.out.println(b.bonjour("le monde"));
+        }
+}
+
+
